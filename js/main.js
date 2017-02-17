9afb3739a471e3b5506b593c33a8f163ceb90190
Jamal Winchester <jamalwinchester@gmail.com>
2017-02-17 01:23:12 -0400

    update to Lanfing Page. Still Under construction

--- js/main.js
+++ js/main.js
@@ -4,6 +4,46 @@ console.log("hello I'm connected to the world");
 //var base_url = "base.php/api";
 
 
+(function( $ ) {
+
+    //Function to animate slider captions 
+    function doAnimations( elems ) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+        
+        elems.each(function () {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+    
+    //Variables on page load 
+    var $myCarousel = $('#carousel-example-generic'),
+        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+        
+    //Initialize carousel 
+    $myCarousel.carousel();
+    
+    //Animate captions in first slide on page load 
+    doAnimations($firstAnimatingElems);
+    
+    //Pause carousel  
+    $myCarousel.carousel('pause');
+    
+    
+    //Other slides to be animated on carousel slide event 
+    $myCarousel.on('slide.bs.carousel', function (e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
+    });  
+    
+})(jQuery);
+
+//*************************************************************NEW
+
 $(document).ready(function(){
     console.log("All Elements in the Page was successfully loaded, we can begin our application logic");
     getTrade();
